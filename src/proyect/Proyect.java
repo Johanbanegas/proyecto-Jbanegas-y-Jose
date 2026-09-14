eda9b8441fdcfd4797f2c1939a39516aa962995f
Johan Banegas <johan_banegas@Unitec.edu>
2026-09-14 17:53:11 -0600

    Iniciando un primitivo sistema de registro.

--- src/proyect/Proyect.java
+++ src/proyect/Proyect.java
@@ -16,56 +16,66 @@ public class Proyect {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String nombre="a";
-        String apellidos="A";
-        String nombrePadre="a";
-        String apellidosPadre="A";
-       int edad=0;
-       int edadPadre=0;
-       String grado="a";
-       String correo="a";
-       String celular="a";
-       String direccion="a";
+        String[] nombre=new String[2];
+        String[] apellidos=new String[2];
+        String[] nombrePadre=new String[2];
+        String[] apellidosPadre=new String[2];
+       int[] edad=new int[2];
+       int[] edadPadre=new int[2];
+       String[] grado=new String[2];
+       String[] correo=new String[2];
+       String[] celular=new String[2];
+       String[] direccion=new String[2];
       Scanner sc=new Scanner(System.in);
-
-       
-//inicio de la plataforma
-System.out.println("-----------PLATAFORMA DE INGRESO DE DATOS-----------");
+ 
+    System.out.println("-----------PLATAFORMA DE INGRESO DE DATOS-----------");
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Alumno:"+(i+1));
+            System.out.println("--------------------------------------------");
+        
         System.out.println("Ingrese el primer y segundo nombre del alumno");
-    nombre=sc.nextLine();
+        nombre[i]=sc.nextLine();
         System.out.println("Ingrese los apellidos del alumno");
-        apellidos=sc.nextLine();
-        System.out.printf("Ingrese la edad de %s ",nombre.toUpperCase());
-        edad=sc.nextInt();
+        apellidos[i]=sc.nextLine();
+        System.out.printf("Ingrese la edad de %s ",nombre[i].toUpperCase());
+        edad[i]=sc.nextInt();
       
         System.out.println("");
  //Datos de los padres 
  sc.nextLine();
         System.out.println("-----------DATOS DEL PADRE-----------");
      System.out.println("Ingrese el primer y segundo nombre del Padre");
-    nombrePadre=sc.nextLine();
-        System.out.printf("Ingrese los apellidos de %s",nombrePadre);
-        apellidosPadre=sc.nextLine();
-        System.out.printf("Ingrese la edad de %s ",nombrePadre.toUpperCase());
-        edadPadre=sc.nextInt();
+    nombrePadre[i]=sc.nextLine();
+        System.out.printf("Ingrese los apellidos de %s",nombrePadre[i]);
+        apellidosPadre[i]=sc.nextLine();
+        System.out.printf("Ingrese la edad de %s ",nombrePadre[i].toUpperCase());
+        edadPadre[i]=sc.nextInt();
+        sc.nextLine();
         System.out.println("Ingrese el correo del padre");
-        correo=sc.nextLine();
+        correo[i]=sc.nextLine();
         System.out.println("Numero de telefono");
-        celular=sc.nextLine();
+        celular[i]=sc.nextLine();
         System.out.println("Proporcione su direccion");
-        direccion=sc.nextLine();
-        
+        direccion[i]=sc.nextLine();
+        }
         System.out.println("---------------RESULTADOS---------------");
         System.out.println("");
-        System.out.println("DATOS DEL ALUMNO");
-        System.out.printf("Su nombre es: %s %s ",nombre.toUpperCase(),apellidos.toUpperCase());
-        System.out.println("Actualmente posee una edad de: "+edad);
+        for (int i = 0; i <2; i++) {
+            
+        
+        System.out.println("DATOS DEL ALUMNO"+(i+1));
+            System.out.println("--------------------------");
+        System.out.printf("Su nombre es: %s %s ",nombre[i].toUpperCase(),apellidos[i].toUpperCase());
+        System.out.println("\nActualmente posee una edad de: "+edad[i]);
         System.out.println("\n DATOS DEL PADRE");
-         System.out.printf("Su nombre es: %s %s ",nombrePadre.toUpperCase(),apellidosPadre.toUpperCase());
-        System.out.println("Actualmente posee una edad de: "+edadPadre);
-        System.out.println("Su direccion es:"+direccion);
-        System.out.println(correo);
+            System.out.println("--------------------------");
+         System.out.printf("Su nombre es: %s %s ",nombrePadre[i].toUpperCase(),apellidosPadre[i].toUpperCase());
+        System.out.println("\nActualmente posee una edad de: "+edadPadre[i]);
+        System.out.println("Su direccion es:"+direccion[i]);
+        System.out.println("Su correo es:"+correo[i]);
+            System.out.println("");
+            System.out.println("--------------------------");
     }
-
+    }
     
 }

78e72c5366d12f42c1cbd51370d3bcd8978299e2
Johan Banegas <johan_banegas@Unitec.edu>
2026-09-17 21:03:32 -0600

    Durante este avance luego de la propuesta del compañero jose de agregar un menu mediante un swich se corrigio los errores resultantes durante la planteacion del avance durante clase.
    al igual se modifico la forma en que se daban los resultados finales en formato similar a una tabla.

--- src/proyect/Proyect.java
+++ src/proyect/Proyect.java
@@ -12,9 +12,7 @@ import java.util.Scanner;
  */
 public class Proyect {
 
-   
-    
-    /* Puse todas las variables aqui porque me estaba dando bastante error 
+  /* Puse todas las variables aqui porque me estaba dando bastante error 
        entonces me toco buscar para ver porque me tiraba error y ahi vi que era mejor
        ponerlas es la class afuera del main y pasarla a static para que todas las funciones y
        arreglos pudieran usarlas sin problema. 
@@ -52,36 +50,80 @@ public class Proyect {
     static int[] ccss = new int[1];
     static int[] cn = new int[1];
     static double[] promedio = new double[1];
+
  
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- 
-        System.out.println("-----------PLATAFORMA DE INGRESO DE DATOS-----------");
+        
+        boolean continuar = true;
+        int seleccionMenu = 0;
+        boolean datosIngresados = false;
+        
+        while (continuar){
+           
+             System.out.println("______________________________________________________");
+              System.out.println("                     MENU PRINCIPAL                   ");
+              System.out.println("______________________________________________________");
+              System.out.println("");
+              System.out.println("---------------------Seleccione una-------------------");
+            System.out.println("");
+        System.out.println("\t\t     1. Ingrese Los datos");
+        System.out.println("\t\t     2. Resultado");
+        System.out.println("\t\t     3. Salir");
+            System.out.println("");
+            System.out.println("---------------Ingrese su opcion escogida-------------");
+            System.out.println("______________________________________________________");
+            System.out.println("======================================================");
+        seleccionMenu = sc.nextInt();
+        sc.nextLine();//limpieza de buffer
+        
+        switch(seleccionMenu) {
+        
+            case 1:
+                System.out.println("");
+        System.out.println("-----------PLATAFORMA DE INGRESO DE DATOS-------------");
  
         for (int i = 0; i < 1; i++) {
             
             System.out.println("Alumno:" + (i + 1));
-            System.out.println("--------------------------------------------");
+            System.out.println("------------------------------------------------------");
  
             DatosdeAlumnos(i);
             DatosdelPadre(i);
             DatosdelaMadre(i);
             DatosAcademicos(i);
- 
+            datosIngresados = true;
+         
         }//finfor
- 
-        MostrarResultados();
+           break;
+           case 2:
+          if (datosIngresados) {//me toco buscar y agregar la varaible datos ingresados y declararlo boolean para que pueda correr el caso 2.
+                    MostrarResultados();
+                } else {
+                    System.out.println("Primero debe ingresar los datos (opcion 1).");
+                }
+        break;
+        
+           case 3:
+                continuar = false;
+                System.out.println("Saliendo del programa...");
+                break;
+        
+        default:
+        System.out.println("No esta la opcion.");
+        
+        }//fin de while
+      
+        }//finde switch menu
+   
  
     }//findemain
  
-   
-    
-    public static void DatosdeAlumnos(int i) {    
-           
-    // FUNCION: datos generales del alumno
-        System.out.println("Ingrese el primer y segundo nombre del alumno");
+    public static void DatosdeAlumnos(int i) {    // FUNCION: datos generales del alumno
+ 
+     System.out.println("Ingrese el primer y segundo nombre del alumno");
         nombre[i] = sc.nextLine();
         System.out.println("Ingrese los apellidos del alumno");
         apellidos[i] = sc.nextLine();
@@ -89,7 +131,7 @@ public class Proyect {
         System.out.printf("Ingrese la edad de %s ", nombre[i].toUpperCase());
         edad[i] = sc.nextInt();
          if (edad[i] < 6 || edad[i] > 17) {
-        System.out.println("ERROR.Debe estar entre 6 y 17 años.");
+        System.out.println("ERROR.Debe estar entre 6 y 17 anios.");
     }
 } while (edad[i] < 6 || edad[i] > 18);
         sc.nextLine();
@@ -103,12 +145,9 @@ public class Proyect {
         fechaNac[i] = sc.nextLine();
         System.out.println("Ingrese el grado: ");
         grado[i] = sc.nextLine();
-        
-        System.out.println("");
-    System.out.println("");
+       
     }//fin DatosdeAlumnos
 
-    
     public static void DatosdelPadre(int i) {   // FUNCION: datos del padre
  
         sc.nextLine();
@@ -117,123 +156,111 @@ public class Proyect {
         nombrePadre[i] = sc.nextLine();
         System.out.printf("Ingrese los apellidos de %s", nombrePadre[i]);
         apellidosPadre[i] = sc.nextLine();
-       do {
-    System.out.printf("Ingrese la edad de %s: ", nombrePadre[i].toUpperCase());
-    edadPadre[i] = sc.nextInt();
-    if (edadPadre[i] < 18) {
-        System.out.println("Error la edad del padre debe ser mayor a 18.");
-    }
-} while (edadPadre[i] < 18);
-sc.nextLine();
+        do {
+            System.out.printf("Ingrese la edad de %s: ", nombrePadre[i].toUpperCase());
+            edadPadre[i] = sc.nextInt();
+            if (edadPadre[i] < 18) {
+                System.out.println("Error la edad del padre debe ser mayor a 18.");
+            }
+        } while (edadPadre[i] < 18);
+        sc.nextLine();
         System.out.println("Ingrese el correo del padre");
         correo[i] = sc.nextLine();
         System.out.println("Numero de telefono");
         celular[i] = sc.nextLine();
         System.out.println("Proporcione su direccion");
         direccion[i] = sc.nextLine();
-      
  
     }//fin DatosdelPadre
  
-    
- 
     public static void DatosdelaMadre(int i) {   // FUNCION: datos de la madre
  
-          sc.nextLine();
+        sc.nextLine();
         System.out.println("-----------DATOS DE LA MADRE-----------");
         System.out.println("Ingrese el primer y segundo nombre de la Madre");
         nombreMadre[i] = sc.nextLine();
         System.out.printf("Ingrese los apellidos de %s", nombreMadre[i]);
         apellidosMadre[i] = sc.nextLine();
         do {
-    System.out.printf("Ingrese la edad de %s: ", nombreMadre[i].toUpperCase());
-    edadMadre[i] = sc.nextInt();
-    if (edadMadre[i] < 18) {
-        System.out.println("Error la edad de la Madre debe ser mayor a 18.");
-    }
-} while (edadPadre[i] < 18);
-sc.nextLine();
+            System.out.printf("Ingrese la edad de %s: ", nombreMadre[i].toUpperCase());
+            edadMadre[i] = sc.nextInt();
+            if (edadMadre[i] < 18) {
+                System.out.println("Error la edad de la Madre debe ser mayor a 18.");
+            }
+        } while (edadPadre[i] < 18);
+        sc.nextLine();
         System.out.println("Ingrese el correo del Madre");
         correoMadre[i] = sc.nextLine();
         System.out.println("Numero de telefono");
         celularMadre[i] = sc.nextLine();
         System.out.println("Proporcione su direccion");
         direccionMadre[i] = sc.nextLine();
-       
  
     }//fin DatosdelaMadre
  
- 
-  
-    public static void DatosAcademicos(int i) {  
-// FUNCION: notas y promedio
+    public static void DatosAcademicos(int i) {  // FUNCION: notas y promedio
  
         System.out.println("-----------------Datos Academicos--------");
         System.out.println("");
         System.out.println("Inserte su nota de:");
-        do{
-        System.out.println("Matematicas: ");
-        math[i] = sc.nextInt();
-        if(math[i]<0||math[i]>100){
-            System.out.println("Su nota esta dentro del alcance permitido");   
-        }
-        }while(math[i]<0||math[i]>100);
-        
-        
-         System.out.println("Inserte su nota de:");
-        do{
-        System.out.println("Ingles: ");
-        ing[i] = sc.nextInt();
-        if(ing[i]<0||ing[i]>100){
-            System.out.println("Su nota esta dentro del alcance permitido");   
-        }
-        }while(ing[i]<0||ing[i]>100);
-        
-        
+        do {
+            System.out.println("Matematicas: ");
+            math[i] = sc.nextInt();
+            if (math[i] < 0 || math[i] > 100) {
+                System.out.println("Su nota esta dentro del alcance permitido");
+            }
+        } while (math[i] < 0 || math[i] > 100);
+
         System.out.println("Inserte su nota de:");
-        do{
-        System.out.println("Computacion: ");
-        comp[i] = sc.nextInt();
-        if(comp[i]<0||comp[i]>100){
-            System.out.println("Su nota esta dentro del alcance permitido");   
-        }
-        }while(comp[i]<0||comp[i]>100);
-       
-        
-   System.out.println("Inserte su nota de:");
-        do{
-        System.out.println("Espaniol: ");
-        esp[i] = sc.nextInt();
-        if(esp[i]<0||esp[i]>100){
-            System.out.println("Su nota esta dentro del alcance permitido");   
-        }
-        }while(esp[i]<0||esp[i]>100);
-       
-        
+        do {
+            System.out.println("Ingles: ");
+            ing[i] = sc.nextInt();
+            if (ing[i] < 0 || ing[i] > 100) {
+                System.out.println("Su nota esta dentro del alcance permitido");
+            }
+        } while (ing[i] < 0 || ing[i] > 100);
+
         System.out.println("Inserte su nota de:");
-        do{
-        System.out.println("Ciencias Sociales: ");
-        ccss[i] = sc.nextInt();
-        if(ccss[i]<0||ccss[i]>100){
-            System.out.println("Su nota esta dentro del alcance permitido");   
-        }
-        }while(ccss[i]<0||ccss[i]>100);
-        
-        
+        do {
+            System.out.println("Computacion: ");
+            comp[i] = sc.nextInt();
+            if (comp[i] < 0 || comp[i] > 100) {
+                System.out.println("Su nota esta dentro del alcance permitido");
+            }
+        } while (comp[i] < 0 || comp[i] > 100);
+
         System.out.println("Inserte su nota de:");
-        do{
-        System.out.println("Ciencias Naturales: ");
-        cn[i] = sc.nextInt();
-        if(cn[i]<0||cn[i]>100){
-            System.out.println("Su nota esta dentro del alcance permitido");   
-        }
-        }while(cn[i]<0||cn[i]>100);
- 
+        do {
+            System.out.println("Espaniol: ");
+            esp[i] = sc.nextInt();
+            if (esp[i] < 0 || esp[i] > 100) {
+                System.out.println("Su nota esta dentro del alcance permitido");
+            }
+        } while (esp[i] < 0 || esp[i] > 100);
+
+        System.out.println("Inserte su nota de:");
+        do {
+            System.out.println("Ciencias Sociales: ");
+            ccss[i] = sc.nextInt();
+            if (ccss[i] < 0 || ccss[i] > 100) {
+                System.out.println("Su nota esta dentro del alcance permitido");
+            }
+        } while (ccss[i] < 0 || ccss[i] > 100);
+
+        System.out.println("Inserte su nota de:");
+        do {
+            System.out.println("Ciencias Naturales: ");
+            cn[i] = sc.nextInt();
+            if (cn[i] < 0 || cn[i] > 100) {
+                System.out.println("Su nota esta dentro del alcance permitido");
+            }
+        } while (cn[i] < 0 || cn[i] > 100);
+
         promedio[i] = (math[i] + ing[i] + comp[i] + esp[i] + ccss[i] + cn[i]) / 6.0;
- sc.nextLine();
+        sc.nextLine();
+ 
     }//fin DatosAcademicos
  
-   
     public static void MostrarResultados() {
  
         System.out.println("---------------RESULTADOS---------------");  //FUNCION: impresion de resultados
@@ -266,46 +293,45 @@ sc.nextLine();
             System.out.println("Su correo es:" + correoMadre[i]);
             System.out.println("");
             
-            System.out.println("-----------Datos Academicos------------");
-            System.out.println("La nota de Matematicas es :" + math[i]);
-            System.out.println("La nota de Ingles es :" + ing[i]);
-            System.out.println("La nota de Computacion es :" + comp[i]);
-            System.out.println("La nota de Espanol es :" + esp[i]);
-            System.out.println("La nota de Ciencias Sociales es :" + ccss[i]);
-            System.out.println("La nota de Ciencias Naturales es :" + cn[i]);
-            System.out.println("");
-            System.out.println("------------------------------------");
-            System.out.printf("el promedio es: %.2f ", promedio[i]);
- 
+           System.out.println("-----------------------------------------DATOS ACADEMICOS-----------------------------------------");
+            System.out.println("Matematicas\tIngles\tComputacion\tEspaniol\tCiencias Sociales\tCiencias Naturales");
+            System.out.printf("%d\t\t%d\t\t%d\t\t%d\t\t\t%d\t\t\t%d\n",
+                    math[i], ing[i], comp[i], esp[i], ccss[i], cn[i]);
+
+            System.out.println("--------------------------------------------------------------------------------------------------");
+            System.out.printf("El promedio es: %.2f\n", promedio[i]);
+
             if (promedio[i] >= 70) {
                 System.out.println("El alumno Aprobo");
             } else {
                 System.out.println("El alumno Reprobo");
             }
-            mostrarRendimiento(promedio[i]);
+            MostrarRendimiento(promedio[i]);
         }//findefor
  
-    }//fin MostrarResultados
-  public static void mostrarRendimiento(double promedio) {
-    if (promedio >= 90) {
-        System.out.println("Rendimiento academico: Excelente");
-    } else {
-        if (promedio >= 70) {
-            System.out.println("Rendimiento academico: Bueno");
-        } else {
-            if (promedio >= 60) {
-                System.out.println("Rendimiento academico: Regular");
-            } else {
-                System.out.println("Rendimiento academico: Deficiente");
-            }//else
-        }//else
-    }//else
+         }//fin MostrarResultados
+         
+    public static void MostrarRendimiento(double promedio){
+             if (promedio >= 90) {
+                 System.out.println("Rendimiento academico: Excelente");
+             } else {
+                 if (promedio >= 70) {
+                     System.out.println("Rendimiento academico: Bueno");
+                 } else {
+                     if (promedio >= 60) {
+                         System.out.println("Rendimiento academico: Regular");
+                     } else {
+                         System.out.println("Rendimiento academico: Deficiente");
+                     }//else
+                 }//else
+             }//else
 
-    // 
-    if (promedio >= 70) {
-        System.out.println("El alumno Aprobo");
-    } else {
-        System.out.println("El alumno Reprobo");
-    }//else
-}//mostrarrendimiento
+             // 
+             if (promedio >= 70) {
+                 System.out.println("El alumno Aprobo");
+             } else {
+                 System.out.println("El alumno Reprobo");
+             }//else
+         }//finMostrarRendimineto
+ 
 }//findeclass
